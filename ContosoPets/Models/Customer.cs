89a19fa0255a701ee36eef4f03f974a976253d90
Apollo013 <paul.j.millar@gmail.com>
2020-07-19 13:28:45 +0100

    Initial Commit

---
+++ ContosoPets/Models/Customer.cs
@@ -0,0 +1,10 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoPets.Models {
+	public class Customer {
+		public int Id;
+
+	}
+}
